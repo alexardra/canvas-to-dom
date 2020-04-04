33dd49728a7bb883bf44b5dd9f847e9123171da4
alexardra <aurus14@freeuni.edu.ge>
2020-04-04 21:55:48 +0400

    Webpack and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js"]
+  },
+  output: {
+    path: __dirname + "/build",
+    publicPath: "/",
+    filename: "app.js"
+  },
+  mode: "development"
+};

fe518ef7aa2a5226e3b6f1c34fac96aed82df288
alexardra <aurus14@freeuni.edu.ge>
2020-04-05 22:53:32 +0400

    Canvas frame to cv.Mat conversion

--- src/index.js
+++ src/index.js
@@ -5,5 +5,16 @@ window.onload = () => {
   cv["onRuntimeInitialized"] = () => {
     // do all your work here
     console.log("opencv loaded");
+
+    canvasToDom("app")
   };
 };
+
+
+// nothing yet, will be added when neccessary
+const sampleOptions = {};
+
+const canvasToDom = (canvasEl, options) => {
+    let src = cv.imread(canvasEl);
+    console.log(src);
+}
